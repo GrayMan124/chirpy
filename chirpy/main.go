cb94ff5f5044d2acaffe2348b3f5db434aaf7266
Antoni Czernek <aczern4@gmail.com>
2025-09-17 21:16:32 +0200

    sql setup

--- chirpy/main.go
+++ chirpy/main.go
@@ -1,21 +1,38 @@
 package main
 
 import (
+	"database/sql"
+	"github.com/GrayMan124/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
 )
 
+import _ "github.com/lib/pq"
+
 type apiConfig struct {
 	fileServerHits atomic.Int32
+	Queries        *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	dbQueries := database.New(db)
+
+	cfg := apiConfig{}
+	cfg.Queries = dbQueries
+	if err != nil {
+		log.Fatal("Failed to connect to DB")
+	}
 	serveMux := http.NewServeMux()
 	server := http.Server{
 		Handler: serveMux,
 		Addr:    ":8080",
 	}
-	cfg := apiConfig{}
 	fileSys := cfg.middleWareMetricsInc(http.FileServer(http.Dir(".")))
 	strip := http.StripPrefix("/app", fileSys)
 	serveMux.Handle("/app/", strip)
